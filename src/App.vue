342b54b835a7e3b3402218a0bb70bc7aa6519024
athirahak <athrhak@gmail.com>
2021-12-15 05:09:37 +0800

    commit files

---
+++ src/App.vue
@@ -0,0 +1,226 @@
+<template>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand" href="#page-top"><img src="./assets/img/logo.png" alt="..." /></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars ms-1"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="#home">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#events">Events</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#classes">Classes</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#tutors">Our Tutors</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container">
+                <div class="masthead-subheading">ROBOTIC ACADEMY</div>
+                <div class="masthead-heading text-uppercase">Develop Digital Talents</div>
+                <a class="btn btn-primary btn-xl text-uppercase" href="#home">Claim Free Trial Class</a>
+            </div>
+        </header>
+        <!-- Home-->
+        <section class="page-section" id="home">
+            <div class="container">
+                <div class="text-center">
+                    <!-- <h2 class="section-heading text-uppercase">Home</h2> -->
+                    <!-- <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3> -->
+                </div>
+                <div class="row text-center">
+                    <div class="col-md-4">
+                        <!-- <h4 class="my-3">E-Commerce</h4> -->
+                        <p class="text-muted">Specially curated modules to provide your child withcoding, robotic & technology courses</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Portfolio Grid-->
+        <div class="feature-section container mx-auto"><div class="
+        badge
+        bg-purple
+        rounded-pill
+        text-start
+        px-5
+        py-4
+        mt-5
+        shadow
+        text-wrap
+      "><p class="lead fw-medium mb-0">YOUR KIDS JOURNEY</p><h3 class="text-primary mb-0">Towards IR4.0 SKILLS EXPERT</h3></div><img src="img/map-image.jpeg" class="w-100 h-auto"><div class="pt-5 text-center mb-5"><a href="/classes" class="btn btn-dark shadow px-4 py-3"> Start their journey now <i class="fas fa-caret-right text-light ms-4"></i></a></div></div>
+        <section class="page-section bg-light" id="portfolio">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">JAZARIAN BENEFITS</h2>
+                    <!-- <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3> -->
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-sm-6 mb-4">
+                        <!-- Portfolio item 1-->
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-bs-toggle="modal" href="#portfolioModal1">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                                </div>
+                                <img class="img-fluid" src="assets/img/assest 9.png" alt="..." />
+                            </a>
+                            <div class="portfolio-caption">
+                                <div class="portfolio-caption-heading">STEM EDUCATION & ROBOTIC CLASS LIKE NO OTHERS</div>
+                                <div class="portfolio-caption-subheading text-muted">Our event and classes are curated for beginners & higher level with guidance and advice from higher institute lecturers & experts with more than 20 years’ experience in STEM related education</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 mb-4">
+                        <!-- Portfolio item 2-->
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-bs-toggle="modal" href="#portfolioModal2">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                                </div>
+                                <img class="img-fluid" src="assets/img/assest 10.png" alt="..." />
+                            </a>
+                            <div class="portfolio-caption">
+                                <div class="portfolio-caption-heading">CERTIFICATION FROM RECOGNIZED BODY IN MALAYSIA
+
+</div>
+                                <div class="portfolio-caption-subheading text-muted">Get certified by a Professional Engineer (accredited by Board of Engineers Malaysia) & MDEC #mydigitalmaker Official Digital Maker Hub (DMH) in Malaysia</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 mb-4">
+                        <!-- Portfolio item 3-->
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-bs-toggle="modal" href="#portfolioModal3">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                                </div>
+                                <img class="img-fluid" src="assets/img/assest 11.png" alt="..." />
+                            </a>
+                            <div class="portfolio-caption">
+                                <div class="portfolio-caption-heading">INDIVIDUAL PERFORMANCE REPORT DASHBOARD</div>
+                                <div class="portfolio-caption-subheading text-muted">Get individual performance through Jazarian Performance Report. They will be assessed via Quarterly Assessment to gauge their understanding</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 mb-4 mb-lg-0">
+                        <!-- Portfolio item 4-->
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-bs-toggle="modal" href="#portfolioModal4">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                                </div>
+                                <img class="img-fluid" src="assets/img/assest 12.png" alt="..." />
+                            </a>
+                            <div class="portfolio-caption">
+                                <div class="portfolio-caption-heading">LEARN FROM THE BEST</div>
+                                <div class="portfolio-caption-subheading text-muted">Our Certified Robotic Trainers will give the BEST learning experience to all Jazarians. They are among the experts to train other teachers in public and private institutions</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 mb-4 mb-sm-0">
+                        <!-- Portfolio item 5-->
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-bs-toggle="modal" href="#portfolioModal5">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                                </div>
+                                <img class="img-fluid" src="assets/img/assest 13.png" alt="..." />
+                            </a>
+                            <div class="portfolio-caption">
+                                <div class="portfolio-caption-heading">INDUSTRY 4WRD CAREER</div>
+                                <div class="portfolio-caption-subheading text-muted">Clear career path towards future INDUSTRY 4WRD. Start exploring IR4.0 knowledge to catapult your children interest towards technology</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <!-- Portfolio item 6-->
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-bs-toggle="modal" href="#portfolioModal6">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                                </div>
+                                <img class="img-fluid" src="assets/img/assest 14.png" alt="..." />
+                            </a>
+                            <div class="portfolio-caption">
+                                <div class="portfolio-caption-heading">ONLINE COURSE</div>
+                                <div class="portfolio-caption-subheading text-muted">Access to our comprehensive Technology Course in our web portal & learn with your own-pace. We provide online class with our Certified Robotic Tutors to help our Jazarian during online class</div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Partners-->
+        <div class="py-5">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-md-3 col-sm-6 my-3">
+                        <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="assets/img/asset 15.png" alt="..." /></a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 my-3">
+                        <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="assets/img/asset 16.png" alt="..." /></a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 my-3">
+                        <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="assets/img/asset 17.png" alt="..." /></a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 my-3">
+                        <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="assets/img/asset 18.png" alt="..." /></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Contact-->
+
+        <div data-v-2b66e61e="" class="shadow-top">
+          <footer data-v-2b66e61e="" class="container mx-auto">
+            <div data-v-2b66e61e="" class="row mx-0 mb-3">
+              <div data-v-2b66e61e="" class="col-12 col-lg-4 py-2">
+                <p data-v-2b66e61e="" class="fw-medium">JAZRO TECHNOLOGY BERHAD<br data-v-2b66e61e="">(1359871-V)</p>
+                <p data-v-2b66e61e="" class="text-nowrap">
+                  <i data-v-2b66e61e="" class="fas fa-at me-2">
+                    </i><a data-v-2b66e61e="" href="mailto:jazarirobot1206@gmail.com" class="text-dark text-decoration-none"> jazarirobot1206@gmail.com </a>
+                    </p><p data-v-2b66e61e="" class="text-nowrap mb-0"><i data-v-2b66e61e="" class="fas fa-phone-square-alt me-2"></i>
+                    <a data-v-2b66e61e="" href="tel:+60185778242" class="text-dark text-decoration-none"> +60185778242 </a></p>
+                    </div><hr data-v-2b66e61e="" class="col-12 my-4 d-block d-lg-none"><div data-v-2b66e61e="" class="col-12 col-md-6 col-lg-4 py-2">
+                      <p data-v-2b66e61e="" class="fw-medium">LINKS</p><p data-v-2b66e61e="" class="text-nowrap">
+                        <a data-v-2b66e61e="" href="/" aria-current="page" class="text-reset text-decoration-none router-link-exact-active router-link-active"> Home </a></p>
+                        <p data-v-2b66e61e="" class="text-nowrap"><a data-v-2b66e61e="" href="/events" class="text-reset text-decoration-none"> Events </a>
+                        </p><p data-v-2b66e61e="" class="text-nowrap"><a data-v-2b66e61e="" href="/classes" class="text-reset text-decoration-none"> Classes </a
+                        ></p><p data-v-2b66e61e="" class="text-nowrap"><a data-v-2b66e61e="" href="/our-tutors" class="text-reset text-decoration-none"> Our Tutors </a>
+                        </p><p data-v-2b66e61e=""><a data-v-2b66e61e="" href="https://www.jazro.com.my/investor" target="_blank" class="btn btn-link p-0 text-decoration-none text-reset"> Invest in JAZRO </a></p><p data-v-2b66e61e="" class="mb-0"><a data-v-2b66e61e="" href="https://www.jazro.com.my/about-us" target="_blank" class="btn btn-link p-0 text-decoration-none text-reset"> About Us </a>
+                        </p></div><hr data-v-2b66e61e="" class="col-12 my-4 d-block d-md-none"><div data-v-2b66e61e="" class="col-12 col-md-6 col-lg-4 py-2">
+                          <p data-v-2b66e61e="" class="fw-medium text-center text-md-start">SOCIAL MEDIA</p>
+                          <div data-v-2b66e61e="" class="social-media text-center text-md-start">
+                            <a data-v-2b66e61e="" href="https://www.youtube.com/channel/UCg5RYpgKWOClY_o22BE5UIw" target="_blank">
+                            <img data-v-2b66e61e="" src="/img/yt_icon.png" alt="Youtube">
+                            </a><a data-v-2b66e61e="" href="https://wa.me/60185778242" target="_blank" class="whatsapp"><img data-v-2b66e61e="" src="/img/whatsapp_icon.png" alt="WhatsApp">
+                            </a><a data-v-2b66e61e="" href="https://www.facebook.com/jazarirobot" target="_blank"><img data-v-2b66e61e="" src="/img/fb_icon.png" alt="Facebook"></a>
+                            <a data-v-2b66e61e="" href="https://www.instagram.com/jazarirobot" target="_blank"><img data-v-2b66e61e="" src="/img/ig_icon.png" alt="Instagram"></a></div></div></div>
+                            </footer><div data-v-2b66e61e="" class="p-1 border-top"></div><div data-v-2b66e61e="" class="row mx-0 p-3"><div data-v-2b66e61e="" class="col-12 col-sm-6 text-sm-start text-center">
+                              <div data-v-2b66e61e="" class="flex navbar-brand me-0"><img data-v-2b66e61e="" src="/img/logo.png" class="logo-img"><img data-v-2b66e61e="" src="/img/logojazro.png" class="logo-text ms-2"></div></div><div data-v-2b66e61e="" class="col-12 col-sm-6 text-center text-sm-end align-self-center"> © 2021 Jazro. All Rights Reserved. </div></div></div>
+        
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
